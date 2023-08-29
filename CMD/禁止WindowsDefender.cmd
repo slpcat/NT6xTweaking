e0d272ed1a7a024fe87543027c41c9d7eb2456e3
若虚 <slpcat@users.noreply.github.com>
2023-08-29 20:09:22 +0800

    Delete 禁止WindowsDefender.cmd

--- CMD/禁止WindowsDefender.cmd
+++
@@ -1,2 +0,0 @@
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Sense" /v Start /t REG_DWORD /d 4
