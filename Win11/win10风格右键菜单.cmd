64ae5f6f612b4d10213e9d6814989d653ea9559e
若虚 <slpcat@users.noreply.github.com>
2023-08-29 20:32:47 +0800

    Create win10风格右键菜单.cmd

---
+++ Win11/win10风格右键菜单.cmd
@@ -0,0 +1,4 @@
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
+rem 恢复win11风格
+rem reg delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
