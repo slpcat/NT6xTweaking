a6715a63a8ac43798d2f1ef709c5df04e577c5cf
若虚 <slpcat@users.noreply.github.com>
2024-04-02 18:22:22 +0800

    Create nt6service_vm.cmd

---
+++ beta/nt6service_vm.cmd
@@ -0,0 +1,15 @@
+sc config BTAGService start= DISABLED
+sc config BluetoothUserService start= DISABLED
+sc config bthserv start= DISABLED
+sc config BthAvctpSvc start= DISABLED
+sc config BthHFSrv start= DISABLED
+
+sc config Spooler start= DISABLED
+sc config WlanSvc start= DISABLED
+sc config Power start= DISABLED
+sc config WbioSrvc start= DISABLED
+sc config WcsPlugInService start= DISABLED
+sc config CaptureService start= DISABLED
+sc config DeviceAssociationService start= DISABLED
+sc config DevicesFlowUserSvc start= DISABLED
+sc config NlaSvc start= DISABLED
