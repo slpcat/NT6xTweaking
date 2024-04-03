1f62c223e2393359d1183a3efe5bf066e7151cef
若虚 <slpcat@users.noreply.github.com>
2024-04-03 13:10:36 +0800

    Update nt6service_desktop.cmd

--- CMD/nt6service_desktop.cmd
+++ CMD/nt6service_desktop.cmd
@@ -248,7 +248,7 @@ sc config IKEEXT start= DISABLED
 rem SERVICE_NAME: iphlpsvc
 rem DISPLAY_NAME: IP Helper
 
-sc config iphlpsvc start= DISABLED
+sc config iphlpsvc start= DEMAND
 
 rem SERVICE_NAME: KeyIso
 rem DISPLAY_NAME: CNG Key Isolation
