ab69e4b29a3543a86b4a10c0859b225ea3407b84
若虚 <slpcat@users.noreply.github.com>
2024-04-09 15:37:44 +0800

    Create blackbird_restore.cmd

---
+++ beta/blackbird_restore.cmd
@@ -0,0 +1,15 @@
+sc config BTAGService start= DEMAND
+sc config BluetoothUserService start= DEMAND
+sc config bthserv start= DEMAND
+sc config BthAvctpSvc start= DEMAND
+sc config BthHFSrv start= DEMAND
+sc config WlanSvc start= AUTO
+sc config DeviceAssociationService start= DEMAND
+sc config DevicesFlowUserSvc start= DEMAND
+
+sc config DoSvc start= DEMAND
+sc config W32Time start= auto
+sc config WpnService start= auto
+sc config WpnUserService start= auto
+
+sc config DPS start= AUTO
